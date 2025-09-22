7a71a16d374225ba28fbbc3eda52d2995fd040a8
enjalz <135357606+enjalz@users.noreply.github.com>
2025-09-22 22:05:52 +0700

    Update main.dart

--- KULIAH/SEM 5/Registrasi/lib/main.dart
+++ KULIAH/SEM 5/Registrasi/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:registrasi/pages/medical_screen.dart';
 import 'package:registrasi/pages/yoga_screen.dart';
 
 void main() {
+  // ini fungsi utama yg pertama kali dijalanin pas app dibuka
   runApp(const HealthApp());
 }
 
@@ -16,30 +17,36 @@ class HealthApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Registrasi dan Navigasi',
+      title: 'Registrasi dan Navigasi', // judul aplikasi
       theme: ThemeData(
+        // atur tema warna aplikasi
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlueAccent),
       ),
-      initialRoute: '/',
+      initialRoute: '/', // halaman pertama yg dituju pas app dibuka
       routes: {
-        '/': (context) => const RegisterPage(),
-        '/home': (context) => const HomeScreen(),
-        '/diet': (context) => const DietPlanScreen(),
-        '/exercises': (context) => const ExercisesScreen(),
-        '/medical': (context) => const MedicalScreen(),
-        '/yoga': (context) => const YogaScreen(),
+        // daftar semua halaman yg bisa diakses
+        '/': (context) => const RegisterPage(), // halaman registrasi
+        '/home': (context) => const HomeScreen(), // halaman utama setelah login
+        '/diet': (context) => const DietPlanScreen(), // halaman diet plan
+        '/exercises': (context) => const ExercisesScreen(), // halaman exercise
+        '/medical': (context) => const MedicalScreen(), // halaman medical info
+        '/yoga': (context) => const YogaScreen(), // halaman yoga
       },
       onGenerateRoute: (settings) {
+        // ini dijalanin kalo ada route yg perlu penanganan khusus
         if (settings.name == '/home') {
+          // ngecek apakah route yg dipanggil itu '/home'
           final args = settings.arguments as RegistrationData?;
           if (args != null) {
+            // kalo ada data registrasi, tampilkan dialog sukses
             WidgetsBinding.instance.addPostFrameCallback((_) {
               RegistrationSuccessDialog.show(context, args);
             });
           }
+          // balik ke halaman home
           return MaterialPageRoute(builder: (_) => const HomeScreen());
         }
-        return null;
+        return null; // kalo ga ketemu route, balik null aja
       },
     );
   }
